17d77262d851c0f1b1008cc5961b5810d1191b01
Calvin Woo <calvinwoo32@gmail.com>
2017-08-10 00:10:21 -0700

    Add CSS

--- src/components/Base/index.js
+++ src/components/Base/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import './styles.css';
 
-const Base = () => <div>Hello World!</div>;
+const Base = () => {
+  return (
+    <div className="base">
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
 
 export default Base;
--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,27 @@ module.exports = {
   entry: {
     app: './src/index.js'
   },
+
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'public')
   },
+
   module: {
     rules: [
-      { test: /\.js|\.jsx$/, exclude: /node_modules/, loader: 'babel-loader' }
+      { test: /\.js|\.jsx$/, exclude: /node_modules/, loader: 'babel-loader' },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', {
+          loader: 'postcss-loader',
+          options: {
+            plugins: [
+              require('postcss-cssnext')()
+            ]
+          }
+        }]
+      }
     ]
   }
+
 };

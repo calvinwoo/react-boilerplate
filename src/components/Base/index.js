7cfc165bf79e1fa25df16c06ad10908f25f93a33
Calvin Woo <calvinwoo32@gmail.com>
2017-09-16 10:58:57 -0700

    Remove routes

--- src/components/Base/index.js
+++ src/components/Base/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
-import Home from '../Home';
-import About from '../About';
 import './styles.css';
 
 const Base = () => {
   return (
-    <Router>
-      <div className="base">
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-      </div>
-    </Router>
+    <div className="base">
+      Hello World!
+    </div>
   );
 };
 
